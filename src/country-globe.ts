25f9b0b6ea16beaaef5ed3c8f0303fd02e113e2f
BicserdiB <balazs.bicserdi@continental-corporation.com>
2020-02-16 22:06:53 +0100

    set orbit function added

--- src/country-globe.ts
+++ src/country-globe.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import $ from "jquery";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import TWEEN from '@tweenjs/tween.js';
 
 export default class CountryGlobe {
   private container: Element;
@@ -16,13 +17,15 @@ export default class CountryGlobe {
   private objLoader: OBJLoader;
 
   private radius: number = 5
-  private mouseDown: boolean = false;
+  private orbitUpdate: boolean = false;
   private mouseX: number = 0;
   private mouseY: number = 0;
   private controls: OrbitControls;
 
   private mapColors = [0xf78786,0x8aec7b, 0xf3f361, 0x6cbaeb, 0xbd8fcf];
   private countryObjects = [] as THREE.Object3D[];
+
+  private orbitCoords = new THREE.Spherical(75, Math.PI/2, 0);
 
   constructor(ct: Element) {
     this.objLoader = new OBJLoader();
@@ -31,11 +34,9 @@ export default class CountryGlobe {
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.camera.position.z = 75;
+    this.camera.position.setFromSpherical(this.orbitCoords);
 
     this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-    this.controls.autoRotate = false;
-    this.controls.enableZoom = true;
 
     this.renderer.setClearColor('#050505');
     this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -60,16 +61,18 @@ export default class CountryGlobe {
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
 
-    const light = new THREE.PointLight(0xffffff, 1, 0, 2);
-    light.position.set(1000, 5, 1500);
-    this.camera.add(light);
+    const light = new THREE.PointLight(0xffffff, 1, 1000);
+    light.position.set(500, 50, 500);
+    this.scene.add(light);
 
-    this.scene.add(this.camera);
+    const light2 = new THREE.AmbientLight(0x808080);
+    this.scene.add(light2);
 
     //this.container.addEventListener('mousemove', this.onMouseMove.bind(this) as any);
-    this.container.addEventListener('mousedown', this.onMouseDown.bind(this) as any);
+    //this.container.addEventListener('mousedown', this.onMouseDown.bind(this) as any);
     //this.container.addEventListener('mouseup', this.onMouseUp.bind(this) as any);
-    //this.container.addEventListener('mousewheel', this.onMouseWheel.bind(this) as any);
+    //this.container.addEventListener('DOMMouseScroll', this.onMouseWheel.bind(this) as any);
+    this.container.addEventListener('keypress', this.onKeypres.bind(this) as any);
 
     this.loadCountriesJson();
     this.render();
@@ -79,7 +82,7 @@ export default class CountryGlobe {
     object.name = (object as any).materialLibraries[0].replace("\.mtl", "");
     this.countryObjects.push(object);
     var color = 0x44ab2b;
-    var ship_material = new THREE.MeshPhongMaterial( { color: color } );
+    var ship_material = new THREE.MeshBasicMaterial( { color: color } );
     object.traverse( function( child ) {
       if ( child instanceof THREE.Mesh ) {
           child.material = ship_material;
@@ -107,16 +110,6 @@ export default class CountryGlobe {
     this.objLoader.load(objFile, this.addCountryBorderOBJ.bind(this));
   }
 
-  private getVertex(longitude: number, latitude: number) : THREE.Vector3{
-    const lambda = longitude * Math.PI / 180;
-    const phi = latitude * Math.PI / 180;
-    return new THREE.Vector3(
-      this.radius * Math.cos(phi) * Math.cos(-lambda),
-      this.radius * Math.sin(phi),
-      this.radius * Math.cos(phi) * Math.sin(-lambda)
-    );
-  }
-
   public loadCountriesJsonData(data: any){
     console.log("Loading country objs and borders countries data");
     for (let coutry in data) {
@@ -140,11 +133,14 @@ export default class CountryGlobe {
     requestAnimationFrame(this.render.bind(this));
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
-  }
+    TWEEN.update();
 
-  private rotateGlobe(deltaX: number, deltaY: number) {
-    this.mesh.rotation.y += deltaX / 100;
-    this.mesh.rotation.x += deltaY / 100;
+    if(this.orbitUpdate){
+      this.camera.position.setFromSpherical(this.orbitCoords);
+    } else {
+      this.orbitCoords.setFromVector3(this.camera.position);
+    }
+    
   }
 
   private getIntersections(){
@@ -165,42 +161,41 @@ export default class CountryGlobe {
   }
 
   private onMouseMove(evt: MouseEvent) {
-    //if (!this.mouseDown) {
-    //  return;
-    //}
-
-    //evt.preventDefault();
-
-    //const deltaX = evt.clientX - this.mouseX;
-    //const deltaY = evt.clientY - this.mouseY;
-    //this.mouseX = evt.clientX;
-    //this.mouseY = evt.clientY;
-    //this.rotateGlobe(deltaX, deltaY);
   }
 
   private onMouseDown(evt: MouseEvent) {
-    evt.preventDefault();
-    this.mouseX = evt.clientX;
-    this.mouseY = evt.clientY;
-    let obj = this.getIntersections();
-    if(obj != null){
-      console.log(obj.name);
-      var select_material = new THREE.MeshPhongMaterial({ color: 0xff0513 });
-      obj.traverse( function( child ) {
-        if ( child instanceof THREE.Mesh ) {
-          child.material = select_material;
-        }
-      });
-    }
   }
 
   private onMouseUp(evt: MouseEvent) {
-    //evt.preventDefault();
-    //this.mouseDown = false;
   }
 
   private onMouseWheel(event: WheelEvent) {
-    //this.camera.position.z += event.deltaY * 0.3;
+  }
+
+  private orbitUpdateDone(){
+    this.orbitUpdate = false;
+  }
+
+  private degrees_to_radians(deg: number)
+  {
+    return deg * (Math.PI/180);
+  }
+
+  public setOrbit(lat: number, lng: number, zoom: number){
+    this.orbitUpdate = true;
+    let sp = new THREE.Spherical(zoom, this.degrees_to_radians((lat * -1) + 90) , this.degrees_to_radians(lng + 90));
+    TWEEN.removeAll();
+    let camTween = new TWEEN.Tween(this.orbitCoords).to(sp, 1000).easing(TWEEN.Easing.Quadratic.InOut);
+    camTween.onComplete(this.orbitUpdateDone.bind(this));
+    camTween.start();
+  }
+
+  private onKeypres(event: Event){
+    this.setOrbit(21.30694,-157.85833, 30);
+    //this.setOrbit(35.6895,139.69171, 30);
+    //this.setOrbit(40.71427,-74.00597,30);
+    //this.setOrbit(-34.90328,-56.18816,30);
+    //this.setOrbit(47.49801,19.03991,30);
   }
 }
 
